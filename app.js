dbdbf1182499ed0e17f58c2eb79af7266a20728f
QodirovDeveloper <startfly58@gmail.com>
2025-06-02 09:58:30 +0500

    add

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// const open = document.getElementById("open");
-// const close = document.getElementById("close");
-// const modal = document.getElementById("modal_overlay");
-// const body = document.body;
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const modal = document.getElementById("modal_overlay");
+const body = document.body;
 
-// open.addEventListener("click", () => {
-//   modal.classList.remove("opacity-0", "pointer-events-none");
-//   modal.classList.add("opacity-100", "pointer-events-auto");
-//   body.classList.add("overflow-hidden"); // Scroll ni o'chirish
-// });
+open.addEventListener("click", () => {
+  modal.classList.remove("opacity-0", "pointer-events-none");
+  modal.classList.add("opacity-100", "pointer-events-auto");
+  body.classList.add("overflow-hidden"); // Scroll ni o'chirish
+});
 
-// close.addEventListener("click", () => {
-//   modal.classList.remove("opacity-100", "pointer-events-auto");
-//   modal.classList.add("opacity-0", "pointer-events-none");
-//   body.classList.remove("overflow-hidden"); // Scroll ni tiklash
-// });
+close.addEventListener("click", () => {
+  modal.classList.remove("opacity-100", "pointer-events-auto");
+  modal.classList.add("opacity-0", "pointer-events-none");
+  body.classList.remove("overflow-hidden"); // Scroll ni tiklash
+});
 
 const container = document.querySelector(".container"),
   mainVideo = container.querySelector("video"),
